439f484b03bf74f0fc5d3b46473b0acc31b28953
dwhenson <dave.henson@gmail.com>
2022-01-22 11:01:26 +0300

    ✨ IMPROVE: solution to focus on only functions needed

--- week-2-friday/app.js
+++ week-2-friday/app.js
@@ -1,10 +1,10 @@
-import { time } from "./time.js";
+import { addDays, getMonth } from "./time.js";
 
 // Create a Date() object for halloween
 const halloween = new Date("October 31, 2021");
 // // returns "Sunday"
-const day = time.getMonth(halloween); // ?
+const day = getMonth(halloween); // ?
 // // // Add 4 months to the dat
-const future = time.addDays(halloween, 4);
+const future = addDays(halloween, 4);
 // // // logs "Thu Mar 03 2022 ..."
 console.log(halloween);
--- week-2-friday/time.js
+++ week-2-friday/time.js
@@ -23,47 +23,45 @@ const days = [
   "Saturday",
 ];
 
-export const time = (function () {
-  const getDay = function (date) {
-    return days[date.getDay()];
-  };
+const getDay = function (date) {
+  return days[date.getDay()];
+};
 
-  const getMonth = function (date) {
-    return months[date.getMonth()];
-  };
+const getMonth = function (date) {
+  return months[date.getMonth()];
+};
 
-  const addSeconds = function (date, number) {
-    return date.setSeconds(date.getSeconds() + number);
-  };
+const addSeconds = function (date, number) {
+  return date.setSeconds(date.getSeconds() + number);
+};
 
-  const addMinutes = function (date, number) {
-    return date.setMinutes(date.getMinutes() + number);
-  };
+const addMinutes = function (date, number) {
+  return date.setMinutes(date.getMinutes() + number);
+};
 
-  const addHours = function (date, number) {
-    return date.setHours(date.getHours() + number);
-  };
+const addHours = function (date, number) {
+  return date.setHours(date.getHours() + number);
+};
 
-  const addDays = function (date, number) {
-    return date.setDate(date.getDate() + number);
-  };
+const addDays = function (date, number) {
+  return date.setDate(date.getDate() + number);
+};
 
-  const addMonths = function (date, number) {
-    return date.setMonth(date.getMonth() + number);
-  };
+const addMonths = function (date, number) {
+  return date.setMonth(date.getMonth() + number);
+};
 
-  const addYears = function (date, number) {
-    return date.setFullYear(date.getFullYear() + number);
-  };
+const addYears = function (date, number) {
+  return date.setFullYear(date.getFullYear() + number);
+};
 
-  return {
-    getDay,
-    getMonth,
-    addSeconds,
-    addMinutes,
-    addHours,
-    addDays,
-    addMonths,
-    addYears,
-  };
-})();
+export {
+  getDay,
+  getMonth,
+  addSeconds,
+  addMinutes,
+  addHours,
+  addDays,
+  addMonths,
+  addYears,
+};
